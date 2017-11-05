cfa94b0da6df6d45bdebfb51edbac0240f04dc99
Bartek Rybak <gotujcygana@gmail.com>
2017-11-05 17:05:54 +0100

    Add files via upload

---
+++ AsyncHttp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+
+namespace Test
+{
+	public class AsyncHttp
+	{
+		private Thread ThreadClient;
+		private WebClient Client;
+		private NameValueCollection Args;
+		private string Adress;
+
+		public delegate void KoniecDelegate(string result);
+		public event KoniecDelegate Finish;
+
+		public delegate void BladDelegate(string error_msg);
+		public event BladDelegate Error;
+
+		private void ThreadClient_Main()
+		{
+			try
+			{
+				string res = Encoding.UTF8.GetString(Client.UploadValues(Adress, Args));
+				Finish(res);
+			}
+			catch (WebException e)
+			{
+				Error(e.Message);
+			}
+		}
+
+		public void Start(string adress,NameValueCollection args)
+		{
+			Args = args;
+			Adress = adress;
+
+			Client = new WebClient();
+
+			ThreadClient = new Thread(new ThreadStart(ThreadClient_Main));
+			ThreadClient.IsBackground = true;
+			ThreadClient.Start();
+		}
+	}
+}
